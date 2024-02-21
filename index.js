2108304ed50a9cf4320292465beeaf6d3074e5df
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2024-02-21 18:54:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function handleMotion(event) {
   const accelerationY = event.accelerationIncludingGravity.y;
 
   // Adjust circle position based on acceleration with higher sensitivity
-  offsetX -= accelerationX * 1; // Increase the scale factor for higher sensitivity
-  offsetY += accelerationY * 1;
+  offsetX -= accelerationX * 10; // Increase the scale factor for higher sensitivity
+  offsetY += accelerationY * 10;
 
   // Limit the movement to the window bounds
   offsetX = Math.min(window.innerWidth - 50, Math.max(0, offsetX));
