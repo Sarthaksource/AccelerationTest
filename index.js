16e8cb1b5b2b12bdf6c737f6c3d3836b06fa8c8d
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2024-02-21 18:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function handleMotion(event) {
     const accelerationX = event.accelerationIncludingGravity.x;
     const accelerationY = event.accelerationIncludingGravity.y;
 
-    const x = accelerationX * 5; // Scale factor for movement
+    const x = accelerationX * 100; // Scale factor for movement
     const y = accelerationY * 5;
     circle.style.left = `${window.innerWidth / 2 + x}px`;
     circle.style.top = `${window.innerHeight / 2 + y}px`;
