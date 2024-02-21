ab240e3d1a93ed91a3d529514d3565ef4b4aca2c
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2024-02-21 18:41:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const circle = document.getElementById('circle');
+window.addEventListener('devicemotion', handleMotion);
+
+function handleMotion(event) {
+    const accelerationX = event.accelerationIncludingGravity.x;
+    const accelerationY = event.accelerationIncludingGravity.y;
+
+    const x = accelerationX * 5; // Scale factor for movement
+    const y = accelerationY * 5;
+    circle.style.left = `${window.innerWidth / 2 + x}px`;
+    circle.style.top = `${window.innerHeight / 2 + y}px`;
+}
